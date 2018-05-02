2b9a35820183f7ce28f46fb111c05c6186220c60
rikten21 <36707198+rikten21@users.noreply.github.com>
2018-05-02 21:25:28 +0200

    Add files via upload
    A harmadik, végső, kellően gyors megoldás.

--- LWVULD-Exercise9.py
+++ LWVULD-Exercise9.py
@@ -1,34 +1,23 @@
 import math
 
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# The prime factors of 600851475143 are 71, 839,  1471 and 6857.
+
 def isPrime(n):
     for i in range(2,n):
-        if n%i==0:
+        if n%i == 0:
             return False
     return True
 
-def primeFactors(n):
-    primefactors = []
-    for i in range(2,n+1):
-        if isPrime(i):
-            if n%i == 0:
-                primefactors.append(i)
-    return primefactors
-
-def biggestNumber(numberlist):
-    max = -math.inf
-    for i in numberlist:
-        if i > max:
-            max = i
-    return max
+def biggestPrimeFactors2(n):
+    div = 1
+    while n != 1:
+        div += 1
+        if isPrime(div):
+            if n%div == 0:
+                n = n/div
+    return div
 
 #main:
-#print(biggestNumber(primeFactors(600851475143)))
-#print(biggestNumber(primeFactors(13195)))
-
-def biggestPrimefactor(n):
-    for i in range(n,0,-1):
-        if n%i == 0:
-            if isPrime(i):
-                return i
-
-#print(biggestPrimefactor(600851475143))
+# print(biggestPrimeFactors2(13195))
+print(biggestPrimeFactors2(600851475143))
