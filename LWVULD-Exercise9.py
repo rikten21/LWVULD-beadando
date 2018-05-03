402519344d681837e0ebc14ab17cf846b396f999
rikten21 <36707198+rikten21@users.noreply.github.com>
2018-05-03 09:33:37 +0200

    Update LWVULD-Exercise9.py

--- LWVULD-Exercise9.py
+++ LWVULD-Exercise9.py
@@ -9,15 +9,33 @@ def isPrime(n):
             return False
     return True
 
-def biggestPrimeFactors2(n):
+def biggestPrimeFactors2(n): 
     div = 1
     while n != 1:
         div += 1
         if isPrime(div):
-            if n%div == 0:
+            while n%div == 0:
                 n = n/div
     return div
 
+def biggestPrimeFactors3(n): #Még gyorsabb, mint a 2-es, mert a 2-t, mint egyetlen páros prímet külön kezeli, így utána csak a páratlanokat vizsgálja.
+    if n <= 1: #Így kezeli azt is, ha 1-et, 0-át vagy negatív egészszámot adunk a függvénynek.
+        return None
+    while n%2 == 0:
+        n = n/2
+    if n == 1:
+        return 2
+    div = 3
+    while n != 1:
+        if isPrime(div):
+            while n%div == 0:
+                n = n/div
+        div += 2
+    return div-2
+
 #main:
 # print(biggestPrimeFactors2(13195))
-print(biggestPrimeFactors2(600851475143))
+# print(biggestPrimeFactors2(600851475143))
+
+# print(biggestPrimeFactors3(13195))
+print(biggestPrimeFactors3(600851475143))
